51884e3c50e9a83c252bc4c5b055116c7ac2295e
Shane Ivan Fallar <ifallar.if@gmail.com>
2022-11-15 22:35:33 +0800

    Minor Code Format Changes

--- JVS_Inventory_System/JVS_Inventory_System/Form1.vb
+++ JVS_Inventory_System/JVS_Inventory_System/Form1.vb
@@ -142,7 +142,6 @@ Public Class Form1
 
     Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
 
-
         '++++++++++++++++ SET MAIN TABLE VALUES ++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
 
